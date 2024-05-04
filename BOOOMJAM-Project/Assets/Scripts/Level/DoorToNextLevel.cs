3cee6ba399ac331c6b9ab81ab80aefe6ddbcc080
zhenfeng <119007497+kszw@users.noreply.github.com>
2024-05-04 11:20:47 -0400

    DoorToNextLevel
    use OnTriggerEnter

--- BOOOMJAM-Project/Assets/Scripts/Level/DoorToNextLevel.cs
+++ BOOOMJAM-Project/Assets/Scripts/Level/DoorToNextLevel.cs
@@ -6,17 +6,23 @@ using UnityEngine.SceneManagement;
 public class DoorToNextLevel : MonoBehaviour
 {
     void Start()
-    {}
-    void Update()
-    {}
-    void EnterNextLevel(Collider collider)
     {
-        Debug.Log("Door");
-        if (collider.gameObject.CompareTag("Player") 
-            && collider.GetType().ToString() == "UnityEngine.CapsuleCollider")
+        // Initialization code if needed
+    }
+
+    void Update()
+    {
+        // Update code if needed
+    }
+
+    // This method gets called when another collider enters the trigger collider attached to this object
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Door triggered by: " + collider.gameObject.name);
+        if (collider.CompareTag("Player") && collider is CapsuleCollider)
         {
+            // Load the next scene by incrementing the current scene's build index
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
-
     }
 }
